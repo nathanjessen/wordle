e7cc93b0dd1455a6be1b06b0a798aec111a503c5
nathanjessen <nathanjessen@users.noreply.github.com>
2022-12-18 20:16:40 +0000

    deploy: ad1e90da24155378b22b57dd2b2123ebbcd31cac

---
+++ assets/ColorTile.stories-00a36a49.js
@@ -0,0 +1,24 @@
+import{C as i}from"./ColorTile-88c211d2.js";import"./jsx-runtime-8ac9c0a6.js";import"./index-10e52e8a.js";import"./_commonjsHelpers-35101cd5.js";const S={title:"Wordle/ColorTile",component:i},r={args:{}},e={args:{children:"a",color:"neutral"}},o={args:{children:"a",color:"info"}},a={args:{children:"a",color:"warning"}},s={args:{children:"a",color:"success"}};var n;r.parameters={...r.parameters,storySource:{source:`{
+  args: {}
+}`,...(n=r.parameters)==null?void 0:n.storySource}};var t;e.parameters={...e.parameters,storySource:{source:`{
+  args: {
+    children: 'a',
+    color: 'neutral'
+  }
+}`,...(t=e.parameters)==null?void 0:t.storySource}};var c;o.parameters={...o.parameters,storySource:{source:`{
+  args: {
+    children: 'a',
+    color: 'info'
+  }
+}`,...(c=o.parameters)==null?void 0:c.storySource}};var l;a.parameters={...a.parameters,storySource:{source:`{
+  args: {
+    children: 'a',
+    color: 'warning'
+  }
+}`,...(l=a.parameters)==null?void 0:l.storySource}};var u;s.parameters={...s.parameters,storySource:{source:`{
+  args: {
+    children: 'a',
+    color: 'success'
+  }
+}`,...(u=s.parameters)==null?void 0:u.storySource}};const y=["Default","Neutral","Info","Warning","Success"];export{r as Default,o as Info,e as Neutral,s as Success,a as Warning,y as __namedExportsOrder,S as default};
+//# sourceMappingURL=ColorTile.stories-00a36a49.js.map
